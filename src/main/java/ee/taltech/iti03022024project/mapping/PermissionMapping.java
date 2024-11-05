8c02774ce8558923d67ab403759be565ce26c615
LValem <lennart.valem@gmail.com>
2024-11-05 22:42:17 +0200

    Fast fixes, because gradle build fails

--- src/main/java/ee/taltech/iti03022024project/mapping/PermissionMapping.java
+++ src/main/java/ee/taltech/iti03022024project/mapping/PermissionMapping.java
@@ -3,10 +3,11 @@ package ee.taltech.iti03022024project.mapping;
 import ee.taltech.iti03022024project.dto.PermissionDto;
 import ee.taltech.iti03022024project.entity.PermissionEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PermissionMapping {
     PermissionDto permissionToDto(PermissionEntity permissionEntity);
     PermissionEntity permissionToEntity(PermissionDto permissionDto);
