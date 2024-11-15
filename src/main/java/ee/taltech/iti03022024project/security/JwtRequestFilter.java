c611bdc0ac7e218a55835a15a7bb455c9ab43d4d
LValem <lennart.valem@gmail.com>
2024-11-15 16:53:21 +0200

    changed security configuration and EmployeeController that only specific permissions can make requests.

--- src/main/java/ee/taltech/iti03022024project/security/JwtRequestFilter.java
+++ src/main/java/ee/taltech/iti03022024project/security/JwtRequestFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -54,8 +55,32 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 .getPayload();
     }
 
+//    private Authentication buildAuthToken(Claims tokenBody) {
+//        return new UsernamePasswordAuthenticationToken(tokenBody.getSubject(),
+//                null, List.of(new SimpleGrantedAuthority("DEFAULT USER")));
+//    }
+
     private Authentication buildAuthToken(Claims tokenBody) {
-        return new UsernamePasswordAuthenticationToken(tokenBody.getSubject(),
-                null, List.of(new SimpleGrantedAuthority("DEFAULT USER")));
+        String username = tokenBody.get("name", String.class);
+        Integer permissionId = tokenBody.get("permissionID", Integer.class);
+        System.out.println(permissionId);
+
+        String role = mapPermissionIdToRole(permissionId);
+        System.out.println(role);
+
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role));
+
+        return new UsernamePasswordAuthenticationToken(username, null, List.of(new SimpleGrantedAuthority(role)));
     }
+
+    private String mapPermissionIdToRole(Integer permissionId) {
+        return switch (permissionId) {
+            case 1 -> "ADMIN";
+            case 2 -> "USER";
+            case 3 -> "DRIVER";
+            default -> "USER";
+        };
+    }
+
+
 }
