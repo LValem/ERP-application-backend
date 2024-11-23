f2db88902c517ad78196efaf9866bad12f149d9b
erbaum <erbaum@taltech.ee>
2024-11-23 20:02:18 +0200

    removed unused import

--- src/main/java/ee/taltech/iti03022024project/repository/PermissionRepository.java
+++ src/main/java/ee/taltech/iti03022024project/repository/PermissionRepository.java
@@ -4,8 +4,6 @@ import ee.taltech.iti03022024project.entity.PermissionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.security.Permission;
-
 @Repository
 public interface PermissionRepository extends JpaRepository<PermissionEntity, Integer> {
     boolean existsByDescriptionIgnoreCase(String description);
